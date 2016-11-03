f3ad8917b0a36bc4cc803fdf95d9759e203b72fa
lkitto <lkitto@gsu.edu>
2016-11-03 14:57:22 -0400

    make dances an array instead of string

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ $(function () {
     console.log("let's go!");
 
     //a person has a name (string), a set of moods(aray of strings), and a set of behaviors(array of objects)
-    function person(name, behaviors, moods, dance) {
+    function person(name, behaviors, moods, dances) {
         this.constructor.population++;
         this.name = name;
         this.behaviors = behaviors;
         this.moods = moods;
         this.current_mood = "unknown";
-        this.dance = dance;
+        this.dances = dances && typeof dances == Array ? dances : ['boogie', 'jig', 'ballet'];
         this.dancing = function () {
-            if (!this.dance) {
-                this.dance = 'boogie';
-            }
-            return " is dancing " + this.dance;
-        };
+            var num = getRandomInt(0, this.dances.length);
+            return " is dancing " + this.dances[num];
+        }.bind(this);
         return this;
     }
+
 
     var jaro_behaviors = [
         {
@@ -25,7 +24,7 @@ $(function () {
             }
         },
         {
-            speak: function (mood) {
+            speak: function () {
                 return "I'm hungry.";
             }
         }
@@ -33,7 +32,7 @@ $(function () {
 
     var jaro_moods = ["happy", "sleepy", "energized", "sick"];
     var jaro = new person("jaro", jaro_behaviors, jaro_moods);
-    jaro.dance = 'ballet';
+    jaro.dances = ['belly dance', 'interpretive dance', 'mosh'];
 
 
     var peter_behaviors = [
@@ -43,12 +42,12 @@ $(function () {
             }
         },
         {
-            speak: function (mood) {
+            speak: function () {
                 return "I'm going skiing.";
             }
         },
         {
-            speak: function (mood) {
+            speak: function () {
                 return "I like to read.";
             }
         }
@@ -73,20 +72,58 @@ $(function () {
     ];
     var lisa_moods = ["happy", "mad", "sad", "surprised"];
 
-    var lisa = person("lisa", lisa_behaviors, lisa_moods, 'hula');
+    var lisa = person("lisa", lisa_behaviors, lisa_moods, ['house', 'disco', 'salsa']);
+
+    var pam_behaviors = [
+        {
+            speak: function (mood) {
+                return "I'm " + mood;
+            }
+        },
+        {
+            speak: function () {
+                return "I'm hungry.";
+            }
+        }
+    ];
+
+    var pam_moods = ["happy", "sleepy", "energized", "sick"];
+    var pam = new person("pam", pam_behaviors, pam_moods);
+    pam.dances = ['jig', 'swing'];
+
+
+    var hetal_behaviors = [
+        {
+            speak: function (mood) {
+                return "I'm " + mood;
+            }
+        },
+        {
+            speak: function () {
+                return "I'm sleepy.";
+            }
+        }
+    ];
+
+    var hetal_moods = ["happy", "sad", "energize", "cool"];
+    var hetal = new person("hetal", hetal_behaviors, hetal_moods);
 
 
     var people = [];
     people.push(jaro);
     people.push(peter);
     people.push(lisa);
+    people.push(pam);
+
     //push your person in the people array
+    people.push(hetal);
 
 
     function setWorldState(people) {
         console.log("fire");
         $.each(people, function (i, person) {
             person.current_mood = person.moods[getRandomInt(0, person.moods.length)];
+
             console.log(person.name + " " + person.current_mood);
         });
         people.map(function (o) {
